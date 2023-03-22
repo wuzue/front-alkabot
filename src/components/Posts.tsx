53fa347e734ba5036402677fbf174893d3d9c75b
Renan Araújo <renan.araujoonline@gmail.com>
2023-03-22 08:55:15 -0300

    created posts display structure

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -18,9 +18,17 @@ function Posts(){
   }, [])
 
   return(<>
-    <div>
+    <div className="grid grid-cols-3 w-[70%] m-auto">
       {posts.map(post => (
-        <p>{post.body}</p>
+        <div className="p-[2rem] m-[1rem] rounded-[1.5rem] border-[1px] shadow-[0_4px_6px_-1px_rgb(0,0,0,0.1),0_2px_4px_-2px_rgb(0,0,0,0.1)] flex flex-col relative">
+          <div className="flex flex-col items-center m-auto">
+            <p className="text-center mb-[1rem] font-bold">{post.title}</p>
+            <p className="mb-[1rem]">{post.body}</p>
+          </div>
+        <div className="absolute bottom-3 right-5">
+          <p className="text-right font-bold">Ler mais</p>
+        </div>
+        </div>
       ))}
     </div>
   </>)
